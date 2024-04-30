69aaf6e08092b6f7a4041ce7a3a59b5b8b9e5328
gabrielwere <weregabriel91@gmail.com>
2024-04-30 11:13:04 +0300

    use timers for led blink delay

--- led_blink/led_blink.asm
+++ led_blink/led_blink.asm
@@ -9,27 +9,22 @@ cbi PORTD,2
 call DELAY
 rjmp REPEAT
 
-
-;create a delay of ~1sec
+;create a delay of ~ 1 sec
 DELAY:
-ldi r16,50
+ldi r16,HIGH(65536-31250); or could be 0x85
+sts TCNT1H,r16
+ldi r17,LOW(65536-31250);or could be 0xee
+sts TCNT1L,r17
+ldi r18,0x00
+sts TCCR1A,r18
+ldi r19,0x04
+sts TCCR1B,r19;pre-scaler of 256,normal mode
 
-L1:
-ldi r17,255
-
-L2:
-ldi r18,255
-
-L3:
-nop
-nop
-dec r18
-brne L3
-
-dec r17
-brne L2
-
-dec r16
-brne L1
-
+AGAIN:
+sbis TIFR1,TOV1
+rjmp AGAIN
+ldi r20,0x00
+sts TCCR1B,r20
+sts TCCR1A,r20
+sbi TIFR1,TOV1
 ret
